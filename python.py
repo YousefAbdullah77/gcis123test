19aaaf7161c8a7a32994348d4f68f4f14a10a178
YousefAbdullah77 <ya9531@g.rit.edu>
2024-09-04 13:38:23 +0400

    version 1

---
+++ python.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+
+print("hello world in single quotes")
+
+print("my name is \"yousef\" ")
+
+print("We are in \"GCIS123\" course, in \"RIT DUBAI\", in morning session")
+print("YOUSEF", "Hamdan")
+
+#name = "yousef")
+
+name= input("enter you name: ")
+print(name)
